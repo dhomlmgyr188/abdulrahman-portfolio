743a133f5443f7964d9f4ca5c18a6c4075ce4b27
dhme <dhme@gmail.com>
2026-03-30 21:13:23 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Import our custom CSS
+import '../styles/main.scss'
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap'
+
+// Import Fontawesome
+import "@fortawesome/fontawesome-free/css/all.css";
+
+// flag icons
+import "flag-icons/css/flag-icons.min.css";
---
+++ vite.config.js
@@ -0,0 +1,61 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+
+export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: resolve("./src/partials"),
+    }),
+  ],
+  root: resolve(__dirname, "src"),
+
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+
+    rollupOptions: {
+      output: {
+        entryFileNames: "js/[name]-[hash].js",
+
+        chunkFileNames: "js/[name]-[hash].js",
+
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name || "";
+          const ext = name.split(".").pop();
+
+          if (ext === "css") {
+            return "css/[name]-[hash][extname]";
+          }
+
+          if (["png", "jpg", "jpeg", "svg", "gif", "webp"].includes(ext)) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+
+          if (["woff", "woff2", "ttf", "eot"].includes(ext)) {
+            return "assets/fonts/[name]-[hash][extname]";
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
+
+  server: {
+    port: 8080,
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          "import",
+          "mixed-decls",
+          "color-functions",
+          "global-builtin",
+        ],
+      },
+    },
+  },
+});
